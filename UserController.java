7a791063a7bf7d9773a577f4cb98ce01d4fc0b26
wjx <18859665780@163.com>
2018-07-08 22:46:01 +0800

    fourth

---
+++ UserController.java
@@ -0,0 +1,33 @@
+//package com.hr.mobile.controller;
+//
+//import com.hr.mobile.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * 员工资料控制器
+// *
+// * @author wjx
+// * @time 2018-02-06
+// */
+//@RestController
+//@RequestMapping("/user")
+//public class UserController {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    /**
+//     * 工时系统新员工录入工时后，推送个人信息到该系统
+//     *
+//     * @param query {"workNumber": "000001", "firstAuditor": "000002,张三", "secondAuditor": "000003,李四"}
+//     * @author wjx
+//     * @time 2018-02-06
+//     */
+//    @RequestMapping("/getAuditUser")
+//    public void addUser(@RequestBody String query) {
+//        userService.addUser(query);
+//    }
+//}
