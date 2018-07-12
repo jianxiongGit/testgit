e464cdab5524c157c1015632f6768cbc69566837
wjx <944957434@qq.com>
2018-07-12 09:05:33 +0800

    修改了

--- Test.java
+++ Test.java
@@ -3,7 +3,9 @@ package com.wdi.hr.transfer;
 public class Test {
 
     public static void main(String args[]){
-		System.out.println("hello world1");
+		for(int i=0; i<100; i++){
+			System.out.println(i);
+		}
         System.out.println("hello world2");
     }
 }
