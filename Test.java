e45783ca767d21787a87578dc02907cec3438d6f
wjx <944957434@qq.com>
2018-07-12 09:15:19 +0800

    ss

--- Test.java
+++ Test.java
@@ -3,7 +3,9 @@ package com.wdi.hr.transfer;
 public class Test {
 
     public static void main(String args[]){
-		System.out.println("hello world1");
+		for(int i=0; i<90; i++){
+			System.out.println(i + 1);
+		}
         System.out.println("hello world2");
     }
 }
