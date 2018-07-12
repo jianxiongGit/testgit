2af8776a1adff3efd03cc0e5302b7384a1ec0275
wjx <944957434@qq.com>
2018-07-12 09:08:37 +0800

    gga

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ package com.wdi.hr.transfer;
 public class Test {
 
     public static void main(String args[]){
-		for(int i=0; i<100; i++){
+		for(int i=0; i<90; i++){
 			System.out.println(i);
 		}
         System.out.println("hello world2");
